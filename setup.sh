c031fef20016b126114157e1c449db3beecc62ff
ombekeellia431-boop <ombekeellia431@gmail.com>
2026-03-01 21:31:17 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,4 @@
 
+#!/bin/bash
+# Ce script modifie la politique de sécurité d'ImageMagick pour autoriser MoviePy
+sudo sed -i 's/domain="coder" rights="none" pattern="PDF"/domain="coder" rights="read|write" pattern="PDF"/' /etc/ImageMagick-6/policy.xml
